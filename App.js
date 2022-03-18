6ec5065b5acebc871f05b3958303e106c3ecb300
NguyenTanTai99-10 <g.stars.killer.12a3@gmail.com>
2022-03-18 17:14:25 +0700

    callvideo

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Constants } from 'react-native-unimodules';
-
+import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import AgoraUIKit from 'agora-rn-uikit';
 
 const App = () => {
-  console.log(Constants.systemFonts);
+  const [videoCall, setVideoCall] = useState(true);
+  const rtcProps = {
+    appId: '49786b1cd3a04cc5afad283a5c2d5e13',
+    channel: 'tai-bao',
+  };
+  const callbacks = {
+    EndCall: () => setVideoCall(true),
+  };
   return (
     <View>
-      <Text>App</Text>
+      {videoCall ? (
+        <TouchableOpacity
+          onPress={() => {
+            setVideoCall(false);
+          }}>
+          <Text>Nhan </Text>
+        </TouchableOpacity>
+      ) : (
+        <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
